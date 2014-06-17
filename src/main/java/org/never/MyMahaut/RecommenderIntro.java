5f1bb7d5b53184e50a5646c3cdfa66f880b632f3
NeverC <xzllc2010@gmail.com>
2014-06-17 21:49:17 +0800

    finishde chapter 03

--- src/main/java/org/never/MyMahaut/RecommenderIntro.java
+++ src/main/java/org/never/MyMahaut/RecommenderIntro.java
@@ -2,15 +2,21 @@ package org.never.MyMahaut;
 
 import java.io.File;
 import java.io.IOException;
+
 import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.eval.DataModelBuilder;
 import org.apache.mahout.cf.taste.eval.RecommenderBuilder;
 import org.apache.mahout.cf.taste.eval.RecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.common.FastByIDMap;
 import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator;
+import org.apache.mahout.cf.taste.impl.model.GenericBooleanPrefDataModel;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.LogLikelihoodSimilarity;
 import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.model.PreferenceArray;
 import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.Recommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
@@ -18,19 +24,27 @@ import org.apache.mahout.common.RandomUtils;
 
 public class RecommenderIntro {
 	public static void main(String[] args) throws IOException, TasteException {
-		RandomUtils.useTestSeed();
-		DataModel model = new FileDataModel(new File("ua.base"));
+		@SuppressWarnings("deprecation")
+		DataModel model = new GenericBooleanPrefDataModel( new FileDataModel(new File("ua.base")));
 		RecommenderEvaluator evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator();
-		RecommenderBuilder builder = new RecommenderBuilder() {
-			@Override
+	
+		RecommenderBuilder recommenderBuilder = new RecommenderBuilder() {
 			public Recommender buildRecommender(DataModel model) throws TasteException {
-				UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
-				UserNeighborhood neighborhood = new NearestNUserNeighborhood(2, similarity, model);
+				UserSimilarity similarity = new LogLikelihoodSimilarity(model);
+				UserNeighborhood neighborhood = new NearestNUserNeighborhood(10, similarity, model);
 				return new GenericUserBasedRecommender(model, neighborhood, similarity);
 			}
 		};
-		double score = evaluator.evaluate(builder, null, model, 0.7, 1.0);
+			
+		DataModelBuilder modelBuilder = new DataModelBuilder() {
+				public DataModel buildDataModel(FastByIDMap<PreferenceArray> trainingData) {
+					return new GenericBooleanPrefDataModel(GenericBooleanPrefDataModel.toDataMap(trainingData));
+			}
+		};
+				
+		double score = evaluator.evaluate(recommenderBuilder, modelBuilder, model, 0.9, 1.0);
 		System.out.println(score);
-
+	
 	}
+	
 }
